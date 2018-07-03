43cf337a4f236efef0cc719debe0f36e6df3008a
Kazuya Ueoka <info@fromkk.info>
2018-07-03 18:22:08 +0900

    handle file command

--- Sources/TestSummaries/Supports/ImageInfo.swift
+++ Sources/TestSummaries/Supports/ImageInfo.swift
@@ -14,7 +14,50 @@ struct ImageInfo {
     var height: Int32?
     
     init?(path: String) {
-        // perform exiftool command
+        self.path = path
+        
+        if let size = ImageInfo.file(with: path) {
+            // first file command
+            self.width = size.width
+            self.height = size.height
+        } else if let size = ImageInfo.exiftool(with: path) {
+            // perform exiftool command
+            self.width = size.width
+            self.height = size.height
+        } else {
+            return nil
+        }
+    }
+    
+    private static func file(with path: String) -> Size? {
+        guard let file = Command.run("which", arguments: ["file"]) else {
+            return nil
+        }
+        
+        guard let result = Command.run(file, arguments: [path]) else {
+            return nil
+        }
+        
+        let elements = result.components(separatedBy: ",")
+        return elements.compactMap({ (element) -> Size? in
+            let element = element.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            let regexp = try! NSRegularExpression(pattern: "^([0-9]+)\\s*x\\s*([0-9]+)$", options: [])
+            let range = (element as NSString).range(of: element)
+            let matches = regexp.matches(in: element, options: [], range: range)
+            
+            guard let match = matches.first else { return nil }
+            
+            let widthRange = match.range(at: 1)
+            let heightRange = match.range(at: 2)
+            
+            let width = Int32((element as NSString).substring(with: widthRange))!
+            let height = Int32((element as NSString).substring(with: heightRange))!
+            
+            return Size(width: width, height: height)
+        }).first
+    }
+    
+    private static func exiftool(with path: String) -> Size? {
         guard let exiftool = Command.run("which", arguments: ["exiftool"]) else {
             return nil
         }
@@ -23,7 +66,7 @@ struct ImageInfo {
             return nil
         }
         
-        self.path = path
+        var size = Size(width: 0, height: 0)
         
         // set width and height
         result.components(separatedBy: "\n").forEach { (line) in
@@ -33,10 +76,12 @@ struct ImageInfo {
             }
             
             if key == "Image Width" {
-                self.width = Int32(value)
+                size.width = Int32(value)!
             } else if key == "Image Height" {
-                self.height = Int32(value)
+                size.height = Int32(value)!
             }
         }
+        
+        return size
     }
 }
